ae2cac5fc3b556e85c284cb82103e2e73c858d14
FrankSauve <franksauve@outlook.com>
2018-11-20 14:24:00 -0500

    Added method to remove accents from words

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import preprocessing
 
-print(preprocessing.get_training_text("trainFR.txt"))
+print(preprocessing.get_training_text("trainOT.txt"))
 
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,16 +1,30 @@
 import re
 import os
+import unicodedata
 
 
 def get_training_text(filename):
     """
     Returns a filtered training dataset
     :param filename: Name of the txt file with the extension
-    :return: filtered_text
+    :return: filtered_words
     """
     with open(os.path.abspath(os.path.join(os.getcwd(), "../input/", filename))) as file:
         text = file.read().replace("\n", '')  # Put the entire text into one big string
-        filtered_text = re.findall("\w+", text)  # Only take the words
+        words = re.findall("[a-zA-Z]+", text)  # Only take the words
 
-    return filtered_text
+        filtered_words = []
+        for word in words:
+            filtered_words.append(remove_accents(word))
 
+    return filtered_words
+
+
+def remove_accents(s):
+    """
+    Removes accents using unicode
+    :param s: Input string
+    :return: The input string without accents
+    """
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
