4fd1ee39da81e689fb8a531bda3236adc80a5a2c
FrankSauve <franksauve@outlook.com>
2018-12-02 14:19:36 -0500

    Added time logging

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import preprocessing
 import ngrams
 import output
 import os
+import time
 
 
 def get_queries(filename):
@@ -143,31 +144,92 @@ while True:
     # Run set of 30 queries
     elif option == "7":
         english_text = preprocessing.get_training_text("trainEN.txt")
+
+        start_time = time.time()
         english_unigram = ngrams.get_unigram(english_text)
+        print("EN unigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         english_bigram = ngrams.get_bigram(english_text)
+        print("EN bigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         english_trigram = ngrams.get_trigram(english_text)
+        print("EN trigram built in: %s seconds" % (time.time() - start_time))
+
         french_text = preprocessing.get_training_text("trainFR.txt")
+
+        start_time = time.time()
         french_unigram = ngrams.get_unigram(french_text)
+        print("FR unigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         french_bigram = ngrams.get_bigram(french_text)
+        print("FR bigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         french_trigram = ngrams.get_trigram(french_text)
+        print("FR trigram built in: %s seconds" % (time.time() - start_time))
+
         german_text = preprocessing.get_training_text("trainOT.txt")
+
+        start_time = time.time()
         german_unigram = ngrams.get_unigram(german_text)
+        print("GE unigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         german_bigram = ngrams.get_bigram(german_text)
+        print("GE bigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         german_trigram = ngrams.get_trigram(german_text)
+        print("GE trigram built in: %s seconds" % (time.time() - start_time))
+
         welsh_text = preprocessing.get_training_text("trainCY.txt")
+
+        start_time = time.time()
         welsh_unigram = ngrams.get_unigram(welsh_text)
+        print("CY unigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         welsh_bigram = ngrams.get_bigram(welsh_text)
+        print("CY bigram built in: %s seconds" % (time.time() - start_time))
+
+        start_time = time.time()
         welsh_trigram = ngrams.get_trigram(welsh_text)
+        print("CY trigram built in: %s seconds" % (time.time() - start_time))
+
+        unigram_avg_time = 0
+        bigram_avg_time = 0
+        trigram_avg_time = 0
 
         # Run all queries on ngrams
         for i, query in enumerate(get_queries("queries.txt")):
 
             print("\nQUERY " + str(i+1) + ": " + query)
 
+            start_time = time.time()
             print(ngrams.query_unigram(query.lower(), english_unigram, french_unigram, german_unigram, welsh_unigram,
                                        "out" + str(i + 1) + ".txt"))
+            print("Unigram query ran in: %s seconds" % (time.time() - start_time))
+            end_time = time.time() - start_time
+            unigram_avg_time += end_time
+
+            start_time = time.time()
             print(ngrams.query_bigram(query.lower(), english_bigram, french_bigram, german_bigram, welsh_bigram,
                                       "out" + str(i + 1) + ".txt"))
+            print("Bigram query ran in: %s seconds" % (time.time() - start_time))
+            end_time = time.time() - start_time
+            bigram_avg_time += end_time
+
+            start_time = time.time()
             print(ngrams.query_trigram(query.lower(), english_trigram, french_trigram, german_trigram, welsh_trigram,
                                        "out" + str(i + 1) + ".txt"))
+            print("Trigram query ran in: %s seconds" % (time.time() - start_time))
+            end_time = time.time() - start_time
+            trigram_avg_time += end_time
+
+        print("\nUnigram average time: %s seconds" % str(unigram_avg_time/30))
+        print("Bigram average time: %s seconds" % str(bigram_avg_time/30))
+        print("Trigram average time: %s seconds" % str(trigram_avg_time/30))
 
